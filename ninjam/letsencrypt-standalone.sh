dcfca56a990bad6e959cf76b64d1fe208cb86657
ghalestrilo <ghalestrilo@gmail.com>
2020-12-08 18:24:56 -0300

    :truck: put this on hold while we try another approach

--- ninjam/letsencrypt-standalone.sh
+++ ninjam/letsencrypt-standalone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-certbot certonly -d HOSTNAME -d www.HOSTNAME -n  --agree-tos --standalone --email HOSTEMAIL
-cp /etc/letsencrypt/live/HOSTNAME/privkey.pem /etc/ninjam/
-cp /etc/letsencrypt/live/HOSTNAME/fullchain.pem /etc/ninjam/
+certbot certonly -d ghales.top -d www.ghales.top -n  --agree-tos --standalone --email ghalestrilo@gmail.com
+cp /etc/letsencrypt/live/ghales.top/privkey.pem /etc/ninjam/
+cp /etc/letsencrypt/live/ghales.top/fullchain.pem /etc/ninjam/
 /etc/init.d/nginx reload
